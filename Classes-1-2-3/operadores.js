276e9e74de6ae4e5a37064bb484964a197a00eb9
manuelCabrera <josemanuelcabrera2011@gmail.com>
2022-08-10 23:52:44 -0500

    all files added

---
+++ Classes-1-2-3/operadores.js
@@ -0,0 +1,82 @@
+let variable = 'valor'
+
+let var1 = '1';
+let var2 = 1;
+
+console.log(var1 +var2)>;
+
+let numero1 = 10;
+let numero2 = -10;
+
+//resta
+console.log(numero1-numero2);
+
+//multiplicaión
+console.log(numero1*numero2);
+
+// division
+//las operaciones entre 0 se recomienda no hacerla para obtener resultados con mayor sentido
+
+console.log(numero1/numero2);
+
+//exponeneciacion
+
+/* ** */
+// calcular la patencia de un numero
+
+console.pow(numero1**numero2);
+
+// tambien se puede utilizar la variableglobal mat que es un objeto y pow que es un metodo
+
+Math.pow();
+
+// modulo 
+
+console.log(numero1%numero2);
+
+// incremental o incremento
+
+//con el incremento antes de la variable tenemos el incremento antes de imprimir
+console.log(++numero1)
+
+//con el incremento despues de la variable tenemos el incremento despues de imprimir
+console.log(numero1++)
+
+//decremental
+
+// al igual que el incremento en el decremnto funciona del mismo modo
+conole.log(--numero1);
+
+conole.log(numero1--);
+
+//operadores logicos
+
+//conjuncion, disyuncion, negación
+
+let  loguueado = false;
+let  tieneTDC = true;
+
+// operador && AND
+console.log(logueagdo && tieneTDC)
+
+// en javascrip hay valores falsesy y truety, tipe coercion, java lo toma como un valor nulo un valor falso
+
+// cuando una variable no esta definida javascript la inicializa ccomo indefinido undefined  asi la incializa javascript
+
+console.log(loguueado && tieneTDC && otravariable);
+
+console.log(loguueado || tieneTDC || otravariable);
+
+comsole.log(!loguueado);
+
+// operadodes con valores numericos
+// >=, <=,
+console.log(10>=5);//true
+console.log(10<=5);//false
+
+// equal normal compara valores se define con ==, en cmabio con extricequal tenemos que tener un igual en valor y tipo de dato si no es asi ttenemos que si no son iguales tanto en tipo de variable como en valor no son iguales
+
+let carro = '1';
+let carro = 1;
+console.log(carro1 == carro2);
+console.log(carro1 === carro2);
---
+++ Classes-1-2-3/tiposDeDatos.js
@@ -0,0 +1,37 @@
+/* Tipos de datos en javascript */
+
+//number
+
+let miEdad = 30;
+
+
+//string
+
+let miNombre = 'manuel cabrera'
+
+/* en los strings tenemos un tipo de string tenemos un dato special mombrado con backsticks o comillas invertidas, esto nos va a dar la posibilidad de hacer la interpolacion de los varoles de las variables para generar una nueva expesion*/
+
+
+let nombreCompleto = `Hola yo soy ${miNombre}`;
+
+console.log(nombreCompleto);
+
+
+// objetos en javascript
+
+let carro = {
+    motor: true,
+    color: 'negro',
+    llantas: 4,
+    puertas:,
+    pasajeros:{
+        manu:{
+            edad: 30,
+            nombre: Jose,
+        }
+    }
+}
+
+// arreglos
+
+let arreglo = [];
